394bd493fd4c07cc4668d5d1d2f0eca2caec30dd
Parikshit Bajpai <parikshit.bajpai@uoit.net>
2018-11-13 13:11:39 -0500

    Updated shell script

--- Assignment_2/run.sh
+++ Assignment_2/run.sh
@@ -1,14 +1,49 @@
+#-------------------Angevin Dynamics of Non-Interacting Particles-----------------------#
+#                                                                                        #
+# This script runs the source codes to reproduce the results presented in the report.    #
+# The script compiles the serial and parallel codes using Intel's ifort compiler and      #
+# finally runs the modified script for computing the velocity correlation.               #
+#                                                                                        #
+#----------------------------------------------------------------------------------------#
+
+
+
 #! /bin/sh
 
 clear
 
 echo ""
-echo "#-----------Langevin Dynamics - Non-Interacting Particles-------------#"
+echo "#--------------Langevin Dynamics - Non-Interacting Particles----------------#"
 echo ""
 echo "Compiler - Intel ifort"
 echo ""
 
+cd bin
 echo "Compiling serial code without optimisation"
-ifort -O0 -o run.x LangDyn_2D_NonInter_BC_VC.f90 && echo "Compilation OK"
-./run.x
+ifort -O0 -o run_s.x ../src/LangDyn_2D_NonInter.f90 && echo "Compilation OK"
+./run_s.x
+echo ""
+echo "Compiling parallel code without optimisation"
+ifort -O0 -fopenmp -o run_p.x ../src/LangDyn_2D_NonInter_OMP.f90 && echo "Compilation OK"
+./run_p.x
+echo ""
+echo "Compiling serial code with aggressive optimisation"
+ifort -O3 -o run_s.x ../src/LangDyn_2D_NonInter.f90 && echo "Compilation OK"
+./run_s.x
+echo ""
+echo "Compiling parallel code with aggressive optimisation"
+ifort -O3 -fopenmp -o run_p.x ../src/LangDyn_2D_NonInter_OMP.f90 && echo "Compilation OK"
+./run_p.x
+
+# Cleaning up the bin folder
+rm *.*
+
+echo ""
+echo "Velocity Correlation - Compiling serial code with aggressive optimisation"
+ifort -O3 -o run_p.x ../src/LangDyn_2D_NonInter_BC_VC.f90 && echo "Compilation OK"
+./run_p.x
+
+echo ""
+echo "----------------------------------------------------------------------------"
+echo ""
 
